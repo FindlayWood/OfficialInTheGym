37fa90a5632e3616d2b5069c90ea4c992a08c58c
Findlay Wood <findlaywood1@gmail.com>
2022-06-19 15:28:00 +0100

    ITG-142 Launch Windows

--- InTheGym/AppDelegate.swift
+++ InTheGym/AppDelegate.swift
@@ -19,16 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-//        onBoard()
         launchScreen()
-//        let navController = UINavigationController()
-//        coordinator = MainCoordinator(navigationController: navController)
-//        coordinator?.start()
-//        
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.rootViewController = navController
-//        window?.makeKeyAndVisible()
-        
         return true
     }
     func onBoard() {
@@ -36,7 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let vc = OnBoardMainViewController()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
-        
     }
     func launchScreen() {
         window = UIWindow(frame: UIScreen.main.bounds)
