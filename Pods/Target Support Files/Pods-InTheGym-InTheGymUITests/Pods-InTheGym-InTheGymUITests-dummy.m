df7239db382c92adc24a02ae70dcdf1325346629
Findlay Wood <findlaywood1@gmail.com>
2022-09-05 20:37:45 +0100

    ITG-151 remove all deleted files

--- Pods/Target Support Files/Pods-InTheGym-InTheGymUITests/Pods-InTheGym-InTheGymUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InTheGym_InTheGymUITests : NSObject
-@end
-@implementation PodsDummy_Pods_InTheGym_InTheGymUITests
-@end
--- Pods/Target Support Files/Pods-InTheGymTests/Pods-InTheGymTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InTheGymTests : NSObject
-@end
-@implementation PodsDummy_Pods_InTheGymTests
-@end
