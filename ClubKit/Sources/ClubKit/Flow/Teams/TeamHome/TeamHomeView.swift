b0fb22554404332f8c8392b026a5defcfb9c4676
FindlayWood <findlaywood1@gmail.com>
2024-01-22 21:35:38 +0000

    make team home just players for now

--- ClubKit/Sources/ClubKit/Flow/Teams/TeamHome/TeamHomeView.swift
+++ ClubKit/Sources/ClubKit/Flow/Teams/TeamHome/TeamHomeView.swift
@@ -27,54 +27,20 @@ struct TeamHomeView: View {
             } header: {
                 Text("Players")
             }
-            Section {
-                Button {
-                    viewModel.selectedAction?(.defaultLineup)
-                } label: {
-                    HStack {
-                        Image(systemName: "text.badge.star")
-                        Text("Default Lineup")
-                    }
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                }
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Image(systemName: "line.3.horizontal")
-                        Text("Lineups")
-                    }
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                }
-            } header: {
-                Text("Lineups")
-            }
-            Section {
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Image(systemName: "calendar")
-                        Text("Fixtures")
-                    }
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                }
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Image(systemName: "calendar.badge.clock")
-                        Text("Results")
-                    }
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                }
-            } header: {
-                Text("Results")
-            }
+//            Section {
+//                Button {
+//                    viewModel.selectedAction?(.defaultLineup)
+//                } label: {
+//                    HStack {
+//                        Image(systemName: "text.badge.star")
+//                        Text("Default Lineup")
+//                    }
+//                    .font(.headline)
+//                    .foregroundColor(.primary)
+//                }
+//            } header: {
+//                Text("Lineups")
+//            }
         }
     }
 }
