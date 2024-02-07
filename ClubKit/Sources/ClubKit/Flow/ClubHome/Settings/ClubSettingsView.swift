3f2f9d7550b44ffc756886265b14ce0c91e0dd1e
FindlayWood <findlaywood1@gmail.com>
2024-02-07 22:14:37 +0000

    add club settings files

---
+++ ClubKit/Sources/ClubKit/Flow/ClubHome/Settings/ClubSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  ClubSettingsView.swift
+//
+//
+//  Created by Findlay Wood on 07/02/2024.
+//
+
+import SwiftUI
+
+struct ClubSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClubSettingsView()
+}
---
+++ ClubKit/Sources/ClubKit/Flow/ClubHome/Settings/ClubSettingsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ClubSettingsViewModel.swift
+//
+//
+//  Created by Findlay Wood on 07/02/2024.
+//
+
+import Foundation
+
+class ClubSettingsViewModel: ObservableObject {
+    
+}
