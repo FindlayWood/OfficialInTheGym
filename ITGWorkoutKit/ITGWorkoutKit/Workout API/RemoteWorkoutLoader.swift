6ffed0a4409088a03cc6f5f1a8df8aa473386640
FindlayWood <findlaywood1@gmail.com>
2024-03-06 16:55:28 +0000

    delivers connectivity error on client error

--- ITGWorkoutKit/ITGWorkoutKit/Workout API/RemoteWorkoutLoader.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout API/RemoteWorkoutLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol FirestoreClient {
-    func get(from path: String)
+    func get(from path: String, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteWorkoutLoader {
@@ -16,13 +16,19 @@ public final class RemoteWorkoutLoader {
     private let client: FirestoreClient
     private let path: String
     
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     public init(client: FirestoreClient, path: String) {
         self.client = client
         self.path = path
     }
     
-    public func load() {
-        client.get(from: path)
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: path) { error in
+            completion(.connectivity)
+        }
     }
 }
 
--- ITGWorkoutKit/ITGWorkoutKitTests/RemoteWorkoutLoaderTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/RemoteWorkoutLoaderTests.swift
@@ -36,6 +36,16 @@ class RemoteWorkoutLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedPaths, [path, path])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteWorkoutLoader.Error?
+        sut.load { error in capturedError = error }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(path: String = "example/path") -> (sut: RemoteWorkoutLoader, client: FirestoreClientSpy) {
@@ -46,8 +56,12 @@ class RemoteWorkoutLoaderTests: XCTestCase {
     
     private class FirestoreClientSpy: FirestoreClient {
         var requestedPaths: [String] = []
+        var error: Error?
         
-        func get(from path: String) {
+        func get(from path: String, completion: @escaping (Error) -> Void) {
+            if let error {
+                completion(error)
+            }
             requestedPaths.append(path)
         }
     }
