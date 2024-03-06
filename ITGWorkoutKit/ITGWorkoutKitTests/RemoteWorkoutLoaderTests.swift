c34a421448ccd10503a58c0d6b978e1c1c0d66b0
FindlayWood <findlaywood1@gmail.com>
2024-03-06 16:33:14 +0000

    guarantee that we can load more than once

--- ITGWorkoutKit/ITGWorkoutKitTests/RemoteWorkoutLoaderTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/RemoteWorkoutLoaderTests.swift
@@ -26,6 +26,16 @@ class RemoteWorkoutLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedPath, path)
     }
     
+    func test_loadTwice_requestDataFromPathTwice() {
+        let path = "example/firestore/path"
+        let (sut, client) = makeSUT(path: path)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedPaths, [path, path])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(path: String = "example/path") -> (sut: RemoteWorkoutLoader, client: FirestoreClientSpy) {
@@ -36,9 +46,11 @@ class RemoteWorkoutLoaderTests: XCTestCase {
     
     private class FirestoreClientSpy: FirestoreClient {
         var requestedPath: String?
+        var requestedPaths: [String] = []
         
         func get(from path: String) {
             requestedPath = path
+            requestedPaths.append(path)
         }
     }
     
