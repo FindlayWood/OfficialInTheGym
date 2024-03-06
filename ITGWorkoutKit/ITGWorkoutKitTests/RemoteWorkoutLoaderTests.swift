191dedd2f1550af158de0f9169590e6aa383ec1c
FindlayWood <findlaywood1@gmail.com>
2024-03-06 19:46:46 +0000

    add more test assertions for different http status codes

--- ITGWorkoutKit/ITGWorkoutKitTests/RemoteWorkoutLoaderTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/RemoteWorkoutLoaderTests.swift
@@ -52,12 +52,19 @@ class RemoteWorkoutLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         
-        var capturedErrors = [RemoteWorkoutLoader.Error]()
-        sut.load { capturedErrors.append($0) }
+        let samples = [199, 201, 300, 400, 500]
         
-        client.complete(withStatusCode: 400)
-        
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        samples.enumerated().forEach { index, code in
+            
+            var capturedErrors = [RemoteWorkoutLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+            
+            capturedErrors = []
+        }
     }
     
     // MARK: - Helpers
