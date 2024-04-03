a9e5a2be06e73eefa2b9149e613665d0c0b70289
FindlayWood <findlaywood1@gmail.com>
2024-04-03 20:12:23 +0100

    Remove redundant test

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout API/URLSessionHTTPClientTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout API/URLSessionHTTPClientTests.swift
@@ -21,16 +21,6 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-        
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
-
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
@@ -47,7 +37,6 @@ class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
 
     private class URLSessionSpy: URLSession {
-        var receivedURLs = [URL]()
         private var stubs = [URL: URLSessionDataTask]()
 
         func stub(url: URL, task: URLSessionDataTask) {
@@ -55,7 +44,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
             return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
