dfabc4f48482d7dd6a27e113c17ba245ad5cf703
FindlayWood <findlaywood1@gmail.com>
2024-04-09 22:26:40 +0100

    Does not delete cache upon LocalFeedLoader creation

---
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,30 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  ITGWorkoutKitTests
+//
+//  Created by Findlay Wood on 09/04/2024.
+//
+
+import XCTest
+
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+final class CacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+}
