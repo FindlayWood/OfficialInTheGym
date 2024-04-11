999024e7f06dd2f4e196a9ffeeca87e8154cf10a
FindlayWood <findlaywood1@gmail.com>
2024-04-11 17:51:50 +0100

    remove redundant test code

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CacheFeedUseCaseTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CacheFeedUseCaseTests.swift
@@ -31,7 +31,6 @@ class FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     
     var deleteCachedFeedCallCount = 0
-    var insertCallCount = 0
     var insertions = [(items: [WorkoutItem], timestamp: Date)]()
     
     private var deletionCompletions = [DeletionCompletion]()
@@ -50,7 +49,6 @@ class FeedStore {
     }
     
     func insert(_ items: [WorkoutItem], timestamp: Date) {
-        insertCallCount += 1
         insertions.append((items, timestamp))
     }
 }
@@ -83,17 +81,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
         sut.save(items)
         store.completeDeletion(with: deletionError)
 
-        XCTAssertEqual(store.insertCallCount, 0)
-    }
-    
-    func test_save_requestsNewCacheInsertionOnSuccessfulDeletion() {
-        let items = [uniqueItem(), uniqueItem()]
-        let (sut, store) = makeSUT()
-
-        sut.save(items)
-        store.completeDeletionSuccessfully()
-
-        XCTAssertEqual(store.insertCallCount, 1)
+        XCTAssertEqual(store.insertions.count, 0)
     }
     
     func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion() {
