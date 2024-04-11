a1ef77585b89264a8ca04f4469796befaebfd80e
FindlayWood <findlaywood1@gmail.com>
2024-04-11 19:23:08 +0100

    Save command succeeds on successful cache insertion

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CacheFeedUseCaseTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CacheFeedUseCaseTests.swift
@@ -62,8 +62,12 @@ class FeedStore {
     }
     
     func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -145,6 +149,24 @@ final class CacheFeedUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
 
         XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        let exp = expectation(description: "Wait for save completion")
+
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+
+        XCTAssertNil(receivedError)
     }
     
     // MARK: - Helpers
