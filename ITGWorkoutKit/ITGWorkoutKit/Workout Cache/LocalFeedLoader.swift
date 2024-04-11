bd2ba2a6789633b583952de90a522f47387e2268
FindlayWood <findlaywood1@gmail.com>
2024-04-11 19:48:27 +0100

    Move LocalFeedLoader (and FeedStore collaborator) to its own file in production

---
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
@@ -0,0 +1,47 @@
+//
+//  LocalFeedLoader.swift
+//  ITGWorkoutKit
+//
+//  Created by Findlay Wood on 11/04/2024.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [WorkoutItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [WorkoutItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+
+            completion(error)
+        }
+    }
+}
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [WorkoutItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CacheFeedUseCaseTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CacheFeedUseCaseTests.swift
@@ -8,45 +8,6 @@
 import XCTest
 import ITGWorkoutKit
 
-class LocalFeedLoader {
-    
-    private let store: FeedStore
-    private let currentDate: () -> Date
-    
-    init(store: FeedStore, currentDate: @escaping () -> Date) {
-        self.store = store
-        self.currentDate = currentDate
-    }
-    
-    func save(_ items: [WorkoutItem], completion: @escaping (Error?) -> Void) {
-        store.deleteCachedFeed { [weak self] error in
-            guard let self = self else { return }
-            
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                self.cache(items, with: completion)
-            }
-        }
-    }
-    
-    private func cache(_ items: [WorkoutItem], with completion: @escaping (Error?) -> Void) {
-        store.insert(items, timestamp: currentDate()) { [weak self] error in
-            guard self != nil else { return }
-
-            completion(error)
-        }
-    }
-}
-
-protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [WorkoutItem], timestamp: Date, completion: @escaping InsertionCompletion)
-}
-
 final class CacheFeedUseCaseTests: XCTestCase {
 
     func test_init_doesNotMessageStoreUponCreation() {
