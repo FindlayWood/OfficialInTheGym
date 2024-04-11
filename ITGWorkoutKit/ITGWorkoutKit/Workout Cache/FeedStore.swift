f065a3f360ea405e4dc36a49991316e3c7ac0c1b
FindlayWood <findlaywood1@gmail.com>
2024-04-11 20:25:13 +0100

    Move LocalFeedItem to its own file

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/FeedStore.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/FeedStore.swift
@@ -15,16 +15,4 @@ public protocol FeedStore {
     func insert(_ items: [LocalWorkoutItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
 
-public struct LocalWorkoutItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let image: URL?
-    
-    public init(id: UUID, description: String?, location: String?, image: URL?) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.image = image
-    }
-}
+
---
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalWorkoutItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalWorkoutItem.swift
+//  ITGWorkoutKit
+//
+//  Created by Findlay Wood on 11/04/2024.
+//
+
+import Foundation
+
+public struct LocalWorkoutItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let image: URL?
+    
+    public init(id: UUID, description: String?, location: String?, image: URL?) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.image = image
+    }
+}
