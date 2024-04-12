26e033fa967bc4a042d83f23a62ed736bd66bf0a
FindlayWood <findlaywood1@gmail.com>
2024-04-12 17:26:17 +0100

    Load command delivers no images on empty cache

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/Helpers/FeedStoreSpy.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/Helpers/FeedStoreSpy.swift
@@ -55,4 +55,8 @@ class FeedStoreSpy: FeedStore {
     func completeRetrieval(with error: Error, at index: Int = 0) {
         retrievalCompletions[index](error)
     }
+    
+    func completeRetrievalWithEmptyCache(at index: Int = 0) {
+        retrievalCompletions[index](nil)
+    }
 }
