6003a63b6b304944851730be9fc71723bec4c9d1
FindlayWood <findlaywood1@gmail.com>
2024-04-12 09:34:35 +0100

    Replace load command completion to return a result type rather than an optional error

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
@@ -13,6 +13,7 @@ public final class LocalFeedLoader {
     private let currentDate: () -> Date
     
     public typealias SaveResult = Error?
+    public typealias LoadResult = LoadWorkoutsResult
     
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -31,8 +32,12 @@ public final class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (Error?) -> Void) {
-        store.retrieve(completion: completion)
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }
     }
     
     private func cache(_ feed: [WorkoutItem], with completion: @escaping (SaveResult) -> Void) {
