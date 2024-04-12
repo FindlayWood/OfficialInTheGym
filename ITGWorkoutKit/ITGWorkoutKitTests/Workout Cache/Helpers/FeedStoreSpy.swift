c2624e1e853003477e8dd7f3f67445fb4e60da99
FindlayWood <findlaywood1@gmail.com>
2024-04-12 09:29:20 +0100

    Load command fails on retrieval error

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/Helpers/FeedStoreSpy.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/Helpers/FeedStoreSpy.swift
@@ -19,6 +19,7 @@ class FeedStoreSpy: FeedStore {
 
     private var deletionCompletions = [DeletionCompletion]()
     private var insertionCompletions = [InsertionCompletion]()
+    private var retrievalCompletions = [RetrievalCompletion]()
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deletionCompletions.append(completion)
@@ -46,7 +47,12 @@ class FeedStoreSpy: FeedStore {
         insertionCompletions[index](nil)
     }
     
-    func retrieve() {
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        retrievalCompletions.append(completion)
         receivedMessages.append(.retrieve)
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](error)
     }
 }
