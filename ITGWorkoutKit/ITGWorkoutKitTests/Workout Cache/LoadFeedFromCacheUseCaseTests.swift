a721b5775c49b9ce426ba8f94b95fd2212f777bd
FindlayWood <findlaywood1@gmail.com>
2024-04-13 10:11:48 +0100

    Load command deletes cache on retrieval error

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LoadFeedFromCacheUseCaseTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -74,6 +74,16 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    
+    func test_load_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+
+        sut.load { _ in }
+        store.completeRetrieval(with: anyNSError())
+
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
     // MARK: - Helpers
 
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
