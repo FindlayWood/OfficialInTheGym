0cb5e257ddda1e4d303104d7c345c166a01e6aaf
FindlayWood <findlaywood1@gmail.com>
2024-04-14 17:56:49 +0100

    Hide cache expiration details from tests with a new DSL method to protect tests from breaking changes

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/Helpers/FeedCacheTestHelpers.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/Helpers/FeedCacheTestHelpers.swift
@@ -19,6 +19,11 @@ func uniqueImageFeed() -> (models: [WorkoutItem], local: [LocalWorkoutItem]) {
 }
 
 extension Date {
+    
+    func minusFeedCacheMaxAge() -> Date {
+        return adding(days: -7)
+    }
+    
     func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
