245d61eb99172002c8078771f4fb3c6cf6270718
FindlayWood <findlaywood1@gmail.com>
2024-04-14 13:43:22 +0100

    LocalFeedLoader does not message store upon creation (before validating the cached feed)

---
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  ITGWorkoutKitTests
+//
+//  Created by Findlay Wood on 14/04/2024.
+//
+
+import XCTest
+import ITGWorkoutKit
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
