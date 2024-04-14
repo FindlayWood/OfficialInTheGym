99982b1c717cddd032676d6c321e0106b802304a
FindlayWood <findlaywood1@gmail.com>
2024-04-14 13:20:54 +0100

    Load command deletes seven days old cache upon retrieval

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
@@ -41,8 +41,12 @@ public final class LocalFeedLoader {
                 completion(.failure(error))
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
+                
+            case .found:
+                self.store.deleteCachedFeed { _ in }
+                completion(.success([]))
 
-            case .found, .empty:
+            case .empty:
                 completion(.success([]))
             }
         }
