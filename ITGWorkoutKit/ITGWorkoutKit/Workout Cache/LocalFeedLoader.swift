d3826ed706ebe98c472364d617aa347edd86d7cd
FindlayWood <findlaywood1@gmail.com>
2024-04-14 14:04:48 +0100

    Extract cache deletion side-effects on expired cache from the load method to the `validateCache` method

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
@@ -44,7 +44,6 @@ public final class LocalFeedLoader {
                 completion(.success(feed.toModels()))
                 
             case .found:
-                self.store.deleteCachedFeed { _ in }
                 completion(.success([]))
 
             case .empty:
@@ -66,7 +65,11 @@ public final class LocalFeedLoader {
             switch result {
             case .failure:
                 self.store.deleteCachedFeed { _ in }
-            default: break
+            
+            case let .found(_, timestamp) where !self.validate(timestamp):
+                self.store.deleteCachedFeed { _ in }
+
+            case .empty, .found: break
             }
         }
     }
