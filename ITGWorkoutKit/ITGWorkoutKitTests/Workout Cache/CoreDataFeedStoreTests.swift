bc68d5b6104b1f712e822e1e9cb6708fec192817
FindlayWood <findlaywood1@gmail.com>
2024-04-16 19:55:21 +0100

    CoreDataFeedStore.deleteCachedFeed() empties previously inserted cache

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CoreDataFeedStoreTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CoreDataFeedStoreTests.swift
@@ -71,7 +71,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 
     func test_delete_emptiesPreviouslyInsertedCache() {
+        let sut = makeSUT()
 
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
 
     func test_storeSideEffects_runSerially() {
