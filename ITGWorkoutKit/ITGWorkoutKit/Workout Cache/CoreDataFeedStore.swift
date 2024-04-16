fd676a2c6b60bbf7d65dee5b9e762f0fe6887cb6
FindlayWood <findlaywood1@gmail.com>
2024-04-16 19:32:42 +0100

    Add private background context to perform store operations

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/CoreDataFeedStore.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/CoreDataFeedStore.swift
@@ -10,9 +10,11 @@ import CoreData
 public final class CoreDataFeedStore: FeedStore {
     
     private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
 
     public init(bundle: Bundle = .main) throws {
         container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+        context = container.newBackgroundContext()
     }
 
     public func retrieve(completion: @escaping RetrievalCompletion) {
