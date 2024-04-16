3da47ea03ae50e5f0186e2da57daefeec5050298
FindlayWood <findlaywood1@gmail.com>
2024-04-16 19:54:05 +0100

    CoreDataFeedStore.deleteCachedFeed() delivers no error on non-empty cache

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CoreDataFeedStoreTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/CoreDataFeedStoreTests.swift
@@ -65,7 +65,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 
     func test_delete_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
 
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
 
     func test_delete_emptiesPreviouslyInsertedCache() {
