c5bb7f5da84ac720d49f5d165e04bb70d5c8eb0a
FindlayWood <findlaywood1@gmail.com>
2024-04-16 20:36:49 +0100

    Add EssentialFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ ITGWorkoutKit/ITGWorkoutKitCacheIntegrationTests/ITGWorkoutKitCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  ITGWorkoutKitCacheIntegrationTests.swift
+//  ITGWorkoutKitCacheIntegrationTests
+//
+//  Created by Findlay Wood on 16/04/2024.
+//
+
+import XCTest
+
+final class ITGWorkoutKitCacheIntegrationTests: XCTestCase {
+
+
+}
