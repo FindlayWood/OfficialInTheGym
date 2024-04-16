084501ec8fe93e6145ca05702e9ebe622049e116
FindlayWood <findlaywood1@gmail.com>
2024-04-16 20:18:13 +0100

    Extract CoreData managed model classes into separate files

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -70,47 +70,6 @@ public final class CoreDataFeedStore: FeedStore {
 
 
 
-@objc(ManagedCache)
-private class ManagedCache: NSManagedObject {
-    @NSManaged var timestamp: Date
-    @NSManaged var feed: NSOrderedSet
-    
-    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
-        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
-        request.returnsObjectsAsFaults = false
-        return try context.fetch(request).first
-    }
-    
-    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
-        try find(in: context).map(context.delete)
-        return ManagedCache(context: context)
-    }
-    
-    var localFeed: [LocalWorkoutItem] {
-        return feed.compactMap { ($0 as? ManagedWorkoutItem)?.local }
-    }
-}
 
-@objc(ManagedWorkoutItem)
-private class ManagedWorkoutItem: NSManagedObject {
-    @NSManaged var id: UUID
-    @NSManaged var imageDescription: String?
-    @NSManaged var location: String?
-    @NSManaged var url: URL?
-    @NSManaged var cache: ManagedCache
-    
-    static func workouts(from localFeed: [LocalWorkoutItem], in context: NSManagedObjectContext) -> NSOrderedSet {
-        return NSOrderedSet(array: localFeed.map { local in
-            let managed = ManagedWorkoutItem(context: context)
-            managed.id = local.id
-            managed.imageDescription = local.description
-            managed.location = local.location
-            managed.url = local.image
-            return managed
-        })
-    }
 
-    var local: LocalWorkoutItem {
-        return LocalWorkoutItem(id: id, description: imageDescription, location: location, image: url)
-    }
-}
+
---
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/Infrastructure/CoreData/ManagedWorkoutItem.swift
@@ -0,0 +1,32 @@
+//
+//  ManagedWorkoutItem.swift
+//  ITGWorkoutKit
+//
+//  Created by Findlay Wood on 16/04/2024.
+//
+
+import CoreData
+
+@objc(ManagedWorkoutItem)
+internal class ManagedWorkoutItem: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL?
+    @NSManaged var cache: ManagedCache
+    
+    internal static func workouts(from localFeed: [LocalWorkoutItem], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedWorkoutItem(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.image
+            return managed
+        })
+    }
+
+    internal var local: LocalWorkoutItem {
+        return LocalWorkoutItem(id: id, description: imageDescription, location: location, image: url)
+    }
+}
