ad376c252adedeb9b8394ea322feffdd462a47c3
FindlayWood <findlaywood1@gmail.com>
2024-04-17 20:54:07 +0100

    Add typealias for FeedStore.DeletionResult and `FeedStore.InsertionResult`

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/FeedStore.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/FeedStore.swift
@@ -10,8 +10,12 @@ import Foundation
 public typealias CachedFeed = (feed: [LocalWorkoutItem], timestamp: Date)
 
 public protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
+    
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
     
     typealias RetrievalResult = Result<CachedFeed?, Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void
