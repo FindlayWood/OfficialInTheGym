b892b53108312255b3a934356884553f806bf84e
FindlayWood <findlaywood1@gmail.com>
2024-04-18 20:01:49 +0100

    Add EssentialFeediOS framework (prod and test) targets for iOS platform-specific components

---
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/ITGWorkoutKitiOSTests.swift
@@ -0,0 +1,13 @@
+//
+//  ITGWorkoutKitiOSTests.swift
+//  ITGWorkoutKitiOSTests
+//
+//  Created by Findlay Wood on 18/04/2024.
+//
+
+import XCTest
+
+final class ITGWorkoutKitiOSTests: XCTestCase {
+
+
+}
