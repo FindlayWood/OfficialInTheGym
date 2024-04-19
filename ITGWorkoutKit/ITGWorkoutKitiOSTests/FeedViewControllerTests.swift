d88f2a46ec97128ca4cb7f400adab4f9ed22f37d
FindlayWood <findlaywood1@gmail.com>
2024-04-19 17:30:31 +0100

    Load feed on view did load

--- ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedViewControllerTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedViewControllerTests.swift
@@ -6,10 +6,20 @@
 //
 
 import XCTest
+import UIKit
 
-final class FeedViewController {
-    init(loader: FeedViewControllerTests.LoaderSpy) {
+final class FeedViewController: UIViewController {
+    private var loader: FeedViewControllerTests.LoaderSpy?
 
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loader?.load()
     }
 }
 
@@ -22,10 +32,24 @@ final class FeedViewControllerTests: XCTestCase {
 
         XCTAssertEqual(loader.loadCallCount, 0)
     }
+    
+    
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
 
     // MARK: - Helpers
 
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
