28d6d6a6729d63da8439211044fb271a1be33619
FindlayWood <findlaywood1@gmail.com>
2024-04-19 17:59:12 +0100

    Load feed on pull to refresh

--- ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedViewControllerTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedViewControllerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import UIKit
 import ITGWorkoutKit
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
     private var loader: WorkoutLoader?
 
     convenience init(loader: WorkoutLoader) {
@@ -19,7 +19,13 @@ final class FeedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
         loader?.load { _ in }
     }
 }
@@ -41,6 +47,15 @@ final class FeedViewControllerTests: XCTestCase {
 
         XCTAssertEqual(loader.loadCallCount, 1)
     }
+    
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+
+        sut.refreshControl?.simulatePullToRefresh()
+
+        XCTAssertEqual(loader.loadCallCount, 2)
+    }
 
     // MARK: - Helpers
     
@@ -57,6 +72,16 @@ final class FeedViewControllerTests: XCTestCase {
         
         func load(completion: @escaping (WorkoutLoader.Result) -> Void) {
             loadCallCount += 1
+        }
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
         }
     }
 }
