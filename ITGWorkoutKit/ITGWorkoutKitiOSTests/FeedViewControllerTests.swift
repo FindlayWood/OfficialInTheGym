b7adcce34b681ffffa4d1214b9a50f2072a69f9c
FindlayWood <findlaywood1@gmail.com>
2024-04-21 13:36:22 +0100

    Extract UIImage test helper to a separate file

--- ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedViewControllerTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedViewControllerTests.swift
@@ -528,17 +528,3 @@ private extension UIRefreshControl {
         }
     }
 }
-
-
-private extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = 1
-
-        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
-            color.setFill()
-            rendererContext.fill(rect)
-        }
-    }
-}
---
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+TestHelpers.swift
+//  ITGWorkoutKitiOSTests
+//
+//  Created by Findlay Wood on 21/04/2024.
+//
+
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+
+        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(rect)
+        }
+    }
+}
