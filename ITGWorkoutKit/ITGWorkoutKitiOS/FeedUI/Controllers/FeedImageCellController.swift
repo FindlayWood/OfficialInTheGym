f6f030cf2a1776b6641f66e6114c6e0043c0b8bc
FindlayWood <findlaywood1@gmail.com>
2024-04-21 17:08:35 +0100

    Extract FeedImageCell creation/configuration and image loading logic with `FeedImageDataLoader` to the new `FeedImageCellController`

---
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,55 @@
+//
+//  FeedImageCellController.swift
+//  ITGWorkoutKitiOS
+//
+//  Created by Findlay Wood on 21/04/2024.
+//
+
+import UIKit
+import ITGWorkoutKit
+
+final class FeedImageCellController {
+    private var task: FeedImageDataLoaderTask?
+    private let model: WorkoutItem
+    private let imageLoader: FeedImageDataLoader
+
+    init(model: WorkoutItem, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+
+    func view() -> UITableViewCell {
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (model.location == nil)
+        cell.locationLabel.text = model.location
+        cell.descriptionLabel.text = model.description
+        cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
+        cell.feedImageContainer.startShimmering()
+
+        let loadImage = { [weak self, weak cell] in
+            guard let self = self else { return }
+
+            self.task = self.imageLoader.loadImageData(from: self.model.image) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.feedImageContainer.stopShimmering()
+            }
+        }
+
+        cell.onRetry = loadImage
+        loadImage()
+
+        return cell
+    }
+
+    func preload() {
+        task = imageLoader.loadImageData(from: model.image) { _ in }
+    }
+
+    deinit {
+        task?.cancel()
+    }
+}
