dbd5c106edb0586fea75086529c5934e0ce64cbd
FindlayWood <findlaywood1@gmail.com>
2024-04-24 21:03:41 +0100

    Replace Closure event handler with delegate Protocol to demonstrate different composition approaches

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
@@ -14,7 +14,7 @@ public final class FeedUIComposer {
     public static func feedComposedWith(feedLoader: WorkoutLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presenter = FeedPresenter()
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
-        let refreshController = FeedRefreshViewController(loadFeed: presentationAdapter.loadFeed)
+        let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let feedController = FeedViewController(refreshController: refreshController)
         presenter.loadingView = WeakRefProxy(refreshController)
         presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
@@ -62,7 +62,7 @@ private final class FeedViewAdapter: FeedView {
     }
 }
 
-private final class FeedLoaderPresentationAdapter {
+private final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDelegate {
     private let feedLoader: WorkoutLoader
     private let presenter: FeedPresenter
 
@@ -71,7 +71,7 @@ private final class FeedLoaderPresentationAdapter {
         self.presenter = presenter
     }
 
-    func loadFeed() {
+    func didRequestFeedRefresh() {
         presenter.didStartLoadingFeed()
 
         feedLoader.load { [weak self] result in
