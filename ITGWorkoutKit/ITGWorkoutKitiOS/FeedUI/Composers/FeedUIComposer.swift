f345c53be986c3e531356069f205d255c2dcb88f
FindlayWood <findlaywood1@gmail.com>
2024-04-24 20:59:32 +0100

    Decouple FeedPresenter from FeedLoader with an adapter in the Composition layer

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
@@ -12,8 +12,9 @@ public final class FeedUIComposer {
     private init() {}
 
     public static func feedComposedWith(feedLoader: WorkoutLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presenter = FeedPresenter(feedLoader: feedLoader)
-        let refreshController = FeedRefreshViewController(loadFeed: presenter.loadFeed)
+        let presenter = FeedPresenter()
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
+        let refreshController = FeedRefreshViewController(loadFeed: presentationAdapter.loadFeed)
         let feedController = FeedViewController(refreshController: refreshController)
         presenter.loadingView = WeakRefProxy(refreshController)
         presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
@@ -57,6 +58,30 @@ private final class FeedViewAdapter: FeedView {
         controller?.tableModel = viewModel.feed.map { model in
             FeedImageCellController(viewModel:
                 FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+        }
+    }
+}
+
+private final class FeedLoaderPresentationAdapter {
+    private let feedLoader: WorkoutLoader
+    private let presenter: FeedPresenter
+
+    init(feedLoader: WorkoutLoader, presenter: FeedPresenter) {
+        self.feedLoader = feedLoader
+        self.presenter = presenter
+    }
+
+    func loadFeed() {
+        presenter.didStartLoadingFeed()
+
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter.didFinishLoadingFeed(with: feed)
+
+            case let .failure(error):
+                self?.presenter.didFinishLoadingFeed(with: error)
+            }
         }
     }
 }
--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Feed Presentation/FeedPresenter.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Feed Presentation/FeedPresenter.swift
@@ -24,24 +24,19 @@ protocol FeedView {
 }
 
 final class FeedPresenter {
-    typealias Observer<T> = (T) -> Void
-
-    private let feedLoader: WorkoutLoader
-
-    init(feedLoader: WorkoutLoader) {
-        self.feedLoader = feedLoader
-    }
-
     var feedView: FeedView?
     var loadingView: FeedLoadingView?
-
-    func loadFeed() {
+    
+    func didStartLoadingFeed() {
         loadingView?.display(FeedLoadingViewModel(isLoading: true))
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.feedView?.display(FeedViewModel(feed: feed))
-            }
-            self?.loadingView?.display(FeedLoadingViewModel(isLoading: false))
-        }
+    }
+    
+    func didFinishLoadingFeed(with feed: [WorkoutItem]) {
+        feedView?.display(FeedViewModel(feed: feed))
+        loadingView?.display(FeedLoadingViewModel(isLoading: false))
+    }
+
+    func didFinishLoadingFeed(with error: Error) {
+        loadingView?.display(FeedLoadingViewModel(isLoading: false))
     }
 }
