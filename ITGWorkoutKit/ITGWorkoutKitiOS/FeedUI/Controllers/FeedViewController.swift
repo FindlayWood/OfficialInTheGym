1b6d5dbcb4c52f0f1eda5e1e72df085f0e3c1973
FindlayWood <findlaywood1@gmail.com>
2024-04-26 22:56:53 +0100

    Move tableView.prefetchDataSource setup to the storyboard

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedViewController.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedViewController.swift
@@ -23,8 +23,6 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.prefetchDataSource = self
-        
         onViewIsAppearing = { vc in
             vc.refresh()
             vc.onViewIsAppearing = nil
