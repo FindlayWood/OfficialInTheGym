60c4e2d0cee109a22de95f73d1774ba4de95e2a0
FindlayWood <findlaywood1@gmail.com>
2024-04-28 15:58:50 +0100

    Localize feed view title string

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Feed Presentation/FeedPresenter.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Feed Presentation/FeedPresenter.swift
@@ -5,6 +5,7 @@
 //  Created by Findlay Wood on 24/04/2024.
 //
 
+import Foundation
 import ITGWorkoutKit
 
 protocol FeedLoadingView {
@@ -25,7 +26,10 @@ final class FeedPresenter {
     }
     
     static var title: String {
-        "My Feed"
+        return NSLocalizedString("FEED_VIEW_TITLE",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Title for the feed view")
     }
     
     func didStartLoadingFeed() {
--- ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
@@ -13,12 +13,17 @@ import ITGWorkoutKitiOS
 final class FeedViewControllerTests: XCTestCase {
     
     func test_feedView_hasTitle() {
-            let (sut, _) = makeSUT()
-
-            sut.loadViewIfNeeded()
-
-            XCTAssertEqual(sut.title, "My Feed")
-        }
+        let (sut, _) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        let bundle = Bundle(for: FeedViewController.self)
+        let localizedKey = "FEED_VIEW_TITLE"
+        let localizedTitle = bundle.localizedString(forKey: localizedKey, value: nil, table: "Feed")
+        
+        XCTAssertNotEqual(localizedKey, localizedTitle, "Missing localized string for key: \(localizedKey)")
+        XCTAssertEqual(sut.title, localizedTitle)
+    }
 
 
     func test_loadFeedActions_requestFeedFromLoader() {
