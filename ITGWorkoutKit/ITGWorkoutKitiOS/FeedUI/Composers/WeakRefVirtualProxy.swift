014c7906e8ae340b40cf51f05905f82c7a649645
FindlayWood <findlaywood1@gmail.com>
2024-04-28 20:34:51 +0100

    Move WeakRefVirtualProxy to separate file

---
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,28 @@
+//
+//  WeakRefVirtualProxy.swift
+//  ITGWorkoutKitiOS
+//
+//  Created by Findlay Wood on 28/04/2024.
+//
+
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
