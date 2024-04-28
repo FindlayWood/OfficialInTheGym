c074ebeb1163f01cc1bfd30f186df3bb4ede5b7d
FindlayWood <findlaywood1@gmail.com>
2024-04-28 20:24:30 +0100

    Move main thread dispatch to the Presenter

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedViewController.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedViewController.swift
@@ -15,15 +15,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     var delegate: FeedViewControllerDelegate?
     
     var tableModel = [FeedImageCellController]() {
-        didSet {
-            if Thread.isMainThread {
-                tableView.reloadData()
-            } else {
-                DispatchQueue.main.async { [weak self] in
-                    self?.tableView.reloadData()
-                }
-            }
-        }
+        didSet { tableView.reloadData() }
     }
     
     private var onViewIsAppearing: ((FeedViewController) -> ())?
@@ -42,10 +34,6 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     func display(_ viewModel: FeedLoadingViewModel) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { [weak self] in self?.display(viewModel) }
-        }
-        
         if viewModel.isLoading {
             refreshControl?.beginRefreshing()
         } else {
