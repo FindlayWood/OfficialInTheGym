5b6228e900ceed9f190a1bb3102dea33339ac4e5
FindlayWood <findlaywood1@gmail.com>
2024-04-28 20:33:11 +0100

    Move MainQueueDispatchDecorator to separate file

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
@@ -26,40 +26,7 @@ public final class FeedUIComposer {
     }
 }
 
-private final class MainQueueDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        
-        completion()
-    }
-}
 
-extension MainQueueDispatchDecorator: WorkoutLoader where T == WorkoutLoader {
-
-    func load(completion: @escaping (WorkoutLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
-
-extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-    
-    func loadImageData(from url: URL?, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
 
 private extension FeedViewController {
     static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
---
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/MainQueueDispatchDecorator.swift
@@ -0,0 +1,44 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  ITGWorkoutKitiOS
+//
+//  Created by Findlay Wood on 28/04/2024.
+//
+
+import Foundation
+import ITGWorkoutKit
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: WorkoutLoader where T == WorkoutLoader {
+
+    func load(completion: @escaping (WorkoutLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
+
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    
+    func loadImageData(from url: URL?, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
