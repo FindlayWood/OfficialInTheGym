66bd0485a1ce51f2eb5cd79d234777ffdc329c99
FindlayWood <findlaywood1@gmail.com>
2024-04-28 20:35:59 +0100

    Move FeedImageDataLoaderPresentationAdapter to separate file

---
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedImageDataLoaderPresentationAdapter.swift
@@ -0,0 +1,40 @@
+//
+//  FeedImageDataLoaderPresentationAdapter.swift
+//  ITGWorkoutKitiOS
+//
+//  Created by Findlay Wood on 28/04/2024.
+//
+
+import ITGWorkoutKit
+
+final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
+    private let model: WorkoutItem
+    private let imageLoader: FeedImageDataLoader
+    private var task: FeedImageDataLoaderTask?
+
+    var presenter: FeedImagePresenter<View, Image>?
+
+    init(model: WorkoutItem, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+
+    func didRequestImage() {
+        presenter?.didStartLoadingImageData(for: model)
+
+        let model = self.model
+        task = imageLoader.loadImageData(from: model.image) { [weak self] result in
+            switch result {
+            case let .success(data):
+                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
+
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingImageData(with: error, for: model)
+            }
+        }
+    }
+
+    func didCancelImageRequest() {
+        task?.cancel()
+    }
+}
--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Composers/FeedUIComposer.swift
@@ -86,35 +86,3 @@ private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
         }
     }
 }
-
-private final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-    private let model: WorkoutItem
-    private let imageLoader: FeedImageDataLoader
-    private var task: FeedImageDataLoaderTask?
-
-    var presenter: FeedImagePresenter<View, Image>?
-
-    init(model: WorkoutItem, imageLoader: FeedImageDataLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-
-    func didRequestImage() {
-        presenter?.didStartLoadingImageData(for: model)
-
-        let model = self.model
-        task = imageLoader.loadImageData(from: model.image) { [weak self] result in
-            switch result {
-            case let .success(data):
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-            }
-        }
-    }
-
-    func didCancelImageRequest() {
-        task?.cancel()
-    }
-}
