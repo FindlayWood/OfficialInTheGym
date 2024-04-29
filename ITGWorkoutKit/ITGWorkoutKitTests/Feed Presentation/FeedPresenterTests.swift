7a451278ab17f036a10be2efe5df2175d5778ffa
FindlayWood <findlaywood1@gmail.com>
2024-04-29 21:48:26 +0100

    Add localized FeedPresenter.title

--- ITGWorkoutKit/ITGWorkoutKitTests/Feed Presentation/FeedPresenterTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Feed Presentation/FeedPresenterTests.swift
@@ -58,6 +58,13 @@ final class FeedPresenter {
         self.errorView = errorView
     }
     
+    static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Title for the feed view")
+    }
+    
     func didStartLoadingFeed() {
         errorView.display(.noError)
     }
@@ -74,6 +81,10 @@ final class FeedPresenter {
 }
 
 class FeedPresenterTests: XCTestCase {
+    
+    func test_title_isLocalized() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
 
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
