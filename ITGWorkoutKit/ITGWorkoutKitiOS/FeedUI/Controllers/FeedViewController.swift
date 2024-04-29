5a451752f2ad42141887f712a0aa05a2be4edc09
FindlayWood <findlaywood1@gmail.com>
2024-04-29 20:53:58 +0100

    Move error view configuration to storyboard

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedViewController.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedViewController.swift
@@ -19,7 +19,7 @@ protocol FeedViewControllerDelegate {
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
     var delegate: FeedViewControllerDelegate?
     
-    public let errorView = ErrorView()
+    @IBOutlet private(set) public var errorView: ErrorView?
 
     
     var tableModel = [FeedImageCellController]() {
@@ -56,7 +56,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     func display(_ viewModel: FeedErrorViewModel) {
-        errorView.message = viewModel.message
+        errorView?.message = viewModel.message
     }
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
