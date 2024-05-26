9f770c8a0e72825cfbbe5bf85f7e9be13f55e606
FindlayWood <findlaywood1@gmail.com>
2024-05-26 21:38:51 +0100

    Move HTTPClient protocol and implementation to standalone folders representing modules

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout API/LoadFeedFromRemoteUseCaseTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout API/LoadFeedFromRemoteUseCaseTests.swift
@@ -171,30 +171,4 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
     }
-    
-    private class ClientSpy: Client {
-        
-        private var messages = [(path: String, completion: (Client.Result) -> Void)]()
-        
-        var requestedPaths: [String] {
-            messages.map { $0.path }
-        }
-        
-        func get(from path: String, completion: @escaping (Client.Result) -> Void) {
-            messages.append((path, completion))
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode statusCode: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(url: URL(string: "Example.com")!,
-                                           statusCode: statusCode,
-                                           httpVersion: nil,
-                                           headerFields: nil)!
-            messages[index].completion(.success((data, response)))
-        }
-    }
-    
 }
