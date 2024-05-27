be0e17db9989218ff3789946f549490bfa270f52
FindlayWood <findlaywood1@gmail.com>
2024-05-27 19:23:34 +0100

    Move test helpers to shared scope

--- ITGWorkoutKit/ITGWorkoutKitTests/Helpers/SharedTestHelpers.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Helpers/SharedTestHelpers.swift
@@ -18,3 +18,14 @@ func anyURL() -> URL {
 func anyData() -> Data {
     return Data("Invalid json".utf8)
 }
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
