999a8ef5b74b7588a0c1cfef5f9d441842fa71a6
FindlayWood <findlaywood1@gmail.com>
2024-05-28 18:07:33 +0100

    Add RemoteFeedImageDataLoader end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.

--- ITGWorkoutKit/WorkoutAPIEndToEndTests/WorkoutAPIEndToEndTests.swift
+++ ITGWorkoutKit/WorkoutAPIEndToEndTests/WorkoutAPIEndToEndTests.swift
@@ -30,6 +30,19 @@ final class WorkoutAPIEndToEndTests: XCTestCase {
             XCTFail("Expected successful feed result, got no result instead")
         }
     }
+    
+    func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
+        switch getFeedImageDataResult() {
+        case let .success(data)?:
+            XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+
+        case let .failure(error)?:
+            XCTFail("Expected successful image data result, got \(error) instead")
+
+        default:
+            XCTFail("Expected successful image data result, got no result instead")
+        }
+    }
 
     // MARK: - Helpers
     
@@ -44,6 +57,25 @@ final class WorkoutAPIEndToEndTests: XCTestCase {
 
         var receivedResult: WorkoutLoader.Result?
         loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+
+        return receivedResult
+    }
+    
+    private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
+        let testServerURL = "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image"
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+
+        let exp = expectation(description: "Wait for load completion")
+
+        var receivedResult: FeedImageDataLoader.Result?
+        _ = loader.loadImageData(from: testServerURL) { result in
             receivedResult = result
             exp.fulfill()
         }
