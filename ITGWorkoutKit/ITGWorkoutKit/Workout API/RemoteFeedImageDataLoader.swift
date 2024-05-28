6b52fa8289fc3166fc8d45b5b1623eec78499ef9
FindlayWood <findlaywood1@gmail.com>
2024-05-28 18:02:58 +0100

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers connectivity error on HTTP client error

--- ITGWorkoutKit/ITGWorkoutKit/Workout API/RemoteFeedImageDataLoader.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout API/RemoteFeedImageDataLoader.swift
@@ -13,6 +13,7 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     
     public enum Error: Swift.Error {
         case invalidData
+        case connectivity
     }
 
     public init(client: Client) {
@@ -57,7 +58,8 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
                 } else {
                     task.complete(with: .failure(Error.invalidData))
                 }
-            case let .failure(error): task.complete(with: .failure(error))
+            case .failure:
+                task.complete(with: .failure(Error.connectivity))
             }
         }
         
