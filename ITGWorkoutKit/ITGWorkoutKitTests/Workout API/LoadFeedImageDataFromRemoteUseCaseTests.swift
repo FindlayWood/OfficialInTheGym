77ff985d321b1e8337fa59c1b94c2698b8b3ed8c
FindlayWood <findlaywood1@gmail.com>
2024-05-28 18:55:32 +0100

    Rename test case class to reflect the use case it refers to

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import ITGWorkoutKit
 
-class RemoteFeedImageDataLoaderTests: XCTestCase {
+class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
 
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
