7dc94b293d96be1dd0f8dea62368f4f28479a64f
FindlayWood <findlaywood1@gmail.com>
2024-05-29 09:12:38 +0100

    LocalFeedImageDataLoader does not message store upon creation

---
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LocalFeedImageDataLoaderTests.swift
@@ -0,0 +1,39 @@
+//
+//  LocalFeedImageDataLoaderTests.swift
+//  ITGWorkoutKitTests
+//
+//  Created by Findlay Wood on 29/05/2024.
+//
+
+import XCTest
+import ITGWorkoutKit
+
+final class LocalFeedImageDataLoader {
+    init(store: Any) {
+
+    }
+}
+
+class LocalFeedImageDataLoaderTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+
+    private class FeedStoreSpy {
+        let receivedMessages = [Any]()
+    }
+
+}
