b2e89ce6d15636fea742a0e40b1f9685dc153537
FindlayWood <findlaywood1@gmail.com>
2024-05-29 19:46:48 +0100

    Rename test case class to clarify intent

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import ITGWorkoutKit
 
-class LocalFeedImageDataLoaderTests: XCTestCase {
+class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
 
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
