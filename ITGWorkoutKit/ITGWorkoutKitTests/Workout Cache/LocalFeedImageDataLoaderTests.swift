764753affe1ef4d0270f23fe8fcd03f0e5bdd76b
FindlayWood <findlaywood1@gmail.com>
2024-05-29 18:01:17 +0100

    LocalFeedImageDataLoader.loadImageData delivers stored data when store finds image data for url

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LocalFeedImageDataLoaderTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LocalFeedImageDataLoaderTests.swift
@@ -34,7 +34,7 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
         store.retrieve(dataForPath: path) { result in
             completion(result
                 .mapError { _ in Error.failed }
-                .flatMap { _ in .failure(Error.notFound) })
+                .flatMap { data in data.map { .success($0) } ?? .failure(Error.notFound) })
         }
         return Task()
     }
@@ -71,6 +71,15 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
 
         expect(sut, toCompleteWith: notFound(), when: {
             store.complete(with: .none)
+        })
+    }
+    
+    func test_loadImageDataFromURL_deliversStoredDataOnFoundData() {
+        let (sut, store) = makeSUT()
+        let foundData = anyData()
+
+        expect(sut, toCompleteWith: .success(foundData), when: {
+            store.complete(with: foundData)
         })
     }
 
