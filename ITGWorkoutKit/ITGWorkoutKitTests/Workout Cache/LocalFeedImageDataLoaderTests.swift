6592d07d292d3116ff62b290264d7a28dbeebf76
FindlayWood <findlaywood1@gmail.com>
2024-05-29 17:58:42 +0100

    LocalFeedImageDataLoader.loadImageData delivers not found error when store can't find image data for url

--- ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LocalFeedImageDataLoaderTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/LocalFeedImageDataLoaderTests.swift
@@ -21,6 +21,7 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
     
     public enum Error: Swift.Error {
         case failed
+        case notFound
     }
 
     private let store: FeedImageDataStore
@@ -31,7 +32,9 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
 
     func loadImageData(from path: String, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         store.retrieve(dataForPath: path) { result in
-            completion(.failure(Error.failed))
+            completion(result
+                .mapError { _ in Error.failed }
+                .flatMap { _ in .failure(Error.notFound) })
         }
         return Task()
     }
@@ -62,6 +65,14 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
             store.complete(with: retrievalError)
         })
     }
+    
+    func test_loadImageDataFromURL_deliversNotFoundErrorOnNotFound() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: notFound(), when: {
+            store.complete(with: .none)
+        })
+    }
 
     // MARK: - Helpers
 
@@ -77,6 +88,10 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         return .failure(LocalFeedImageDataLoader.Error.failed)
     }
 
+    private func notFound() -> FeedImageDataLoader.Result {
+        return .failure(LocalFeedImageDataLoader.Error.notFound)
+    }
+    
     private func expect(_ sut: LocalFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
 
@@ -117,6 +132,9 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         func complete(with error: Error, at index: Int = 0) {
             completions[index](.failure(error))
         }
+        
+        func complete(with data: Data?, at index: Int = 0) {
+            completions[index](.success(data))
+        }
     }
-
 }
