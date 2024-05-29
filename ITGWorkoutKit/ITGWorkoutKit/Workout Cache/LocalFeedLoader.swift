2f1d90454eba13f952ad3b1197aa8c818ef36c9c
FindlayWood <findlaywood1@gmail.com>
2024-05-29 21:09:41 +0100

    LocalFeedLoader in integration with the CoreDataFeedStore does not delete recently saved items saved by separate instances when validating cache, proving we correctly manage the data models on disk.

--- ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Workout Cache/LocalFeedLoader.swift
@@ -69,18 +69,21 @@ extension LocalFeedLoader: WorkoutLoader {
 
 extension LocalFeedLoader {
     
-    public func validateCache() {
+    public typealias ValidationResult = Result<Void, Error>
+    
+    public func validateCache(completion: @escaping (ValidationResult) -> Void = { _ in }) {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .failure:
-                self.store.deleteCachedFeed { _ in }
+                self.store.deleteCachedFeed { _ in completion(.success(())) }
             
             case let .success(.some(cache)) where !FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
-                self.store.deleteCachedFeed { _ in }
+                self.store.deleteCachedFeed { _ in completion(.success(())) }
 
-            case .success: break
+            case .success:
+                completion(.success(()))
             }
         }
     }
--- ITGWorkoutKit/ITGWorkoutKitCacheIntegrationTests/ITGWorkoutKitCacheIntegrationTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitCacheIntegrationTests/ITGWorkoutKitCacheIntegrationTests.swift
@@ -52,6 +52,18 @@ final class ITGWorkoutKitCacheIntegrationTests: XCTestCase {
 
         expect(feedLoaderToPerformLoad, toLoad: latestFeed)
     }
+    
+    
+    func test_validateFeedCache_doesNotDeleteRecentlySavedFeed() {
+        let feedLoaderToPerformSave = makeFeedLoader()
+        let feedLoaderToPerformValidation = makeFeedLoader()
+        let feed = uniqueImageFeed().models
+
+        save(feed, with: feedLoaderToPerformSave)
+        validateCache(with: feedLoaderToPerformValidation)
+
+        expect(feedLoaderToPerformSave, toLoad: feed)
+    }
 
     // MARK: - LocalFeedImageDataLoader Tests
     
@@ -110,6 +122,17 @@ final class ITGWorkoutKitCacheIntegrationTests: XCTestCase {
         loader.save(feed) { result in
             if case let Result.failure(error) = result {
                 XCTFail("Expected to save feed successfully, got error: \(error)", file: file, line: line)
+            }
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+    }
+    
+    private func validateCache(with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
+        let saveExp = expectation(description: "Wait for save completion")
+        loader.validateCache() { result in
+            if case let Result.failure(error) = result {
+                XCTFail("Expected to validate feed successfully, got error: \(error)", file: file, line: line)
             }
             saveExp.fulfill()
         }
