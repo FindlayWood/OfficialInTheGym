71ad550b97a28586f0e17c74b8398518b6db214a
FindlayWood <findlaywood1@gmail.com>
2024-05-29 19:49:23 +0100

    Extract FeedImageDataStoreSpy into a shared scope

---
+++ ITGWorkoutKit/ITGWorkoutKitTests/Workout Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -0,0 +1,37 @@
+//
+//  FeedImageDataStoreSpy.swift
+//  ITGWorkoutKitTests
+//
+//  Created by Findlay Wood on 29/05/2024.
+//
+
+import Foundation
+import ITGWorkoutKit
+
+class FeedImageDataStoreSpy: FeedImageDataStore {
+    enum Message: Equatable {
+        case insert(data: Data, for: String)
+        case retrieve(dataFor: String)
+    }
+
+    private(set) var receivedMessages = [Message]()
+    
+    private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    
+    func insert(_ data: Data, for path: String, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        receivedMessages.append(.insert(data: data, for: path))
+    }
+
+    func retrieve(dataForPath path: String, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        receivedMessages.append(.retrieve(dataFor: path))
+        retrievalCompletions.append(completion)
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](.failure(error))
+    }
+    
+    func completeRetrieval(with data: Data?, at index: Int = 0) {
+        retrievalCompletions[index](.success(data))
+    }
+}
