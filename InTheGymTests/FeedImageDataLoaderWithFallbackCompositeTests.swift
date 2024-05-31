b666c5c086516a72bff0a8b9f3b6066cbc9b5e1d
FindlayWood <findlaywood1@gmail.com>
2024-05-31 22:35:15 +0100

    FeedImageDataLoaderWithFallbackComposite.loadImageData loads from primary loader first

--- InTheGymTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ InTheGymTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -10,17 +10,21 @@ import XCTest
 import ITGWorkoutKit
 
 class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    
+    private let primary: FeedImageDataLoader
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
+    }
+    
     private class Task: FeedImageDataLoaderTask {
         func cancel() {
 
         }
     }
 
-    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-
-    }
-
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        _ = primary.loadImageData(from: url) { _ in }
         return Task()
     }
 }
@@ -35,8 +39,24 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
+    
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+        _ = sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
 
     // MARK: - Helpers
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
+    }
 
     private class LoaderSpy: FeedImageDataLoader {
         private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
