8e20f3bfa0952bf137f9932c2f82b5b50f81f762
FindlayWood <findlaywood1@gmail.com>
2024-05-31 22:58:17 +0100

    Move FeedImageDataLoaderWithFallbackComposite to production

--- InTheGymTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ InTheGymTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -8,40 +8,7 @@
 
 import XCTest
 import ITGWorkoutKit
-
-class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
-    
-    private let primary: FeedImageDataLoader
-    private let fallback: FeedImageDataLoader
-    
-    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    private class TaskWrapper: FeedImageDataLoaderTask {
-        var wrapped: FeedImageDataLoaderTask?
-        
-        func cancel() {
-            wrapped?.cancel()
-        }
-    }
-
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = TaskWrapper()
-        task.wrapped = primary.loadImageData(from: url) { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-
-            case .failure:
-                task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
-            }
-
-        }
-        return task
-    }
-}
+import InTheGym
 
 class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
 
