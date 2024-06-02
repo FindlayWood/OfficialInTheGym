2a16a46dec63a25930be2d3be082d4937a8dce54
FindlayWood <findlaywood1@gmail.com>
2024-06-02 17:27:11 +0100

    Move FeedImageDataCache to the Feed Feature production group

--- InTheGymTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ InTheGymTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -10,12 +10,6 @@ import XCTest
 import ITGWorkoutKit
 import InTheGym
 
-protocol FeedImageDataCache {
-    typealias Result = Swift.Result<Void, Error>
-
-    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
-}
-
 class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
     private let decoratee: FeedImageDataLoader
     private let cache: FeedImageDataCache
