84e232f1f5d6b1f9a0738a69a2820c03983b606d
FindlayWood <findlaywood1@gmail.com>
2024-06-02 15:17:31 +0100

    FeedLoaderCacheDecorator.load delivers decoratee loader result (either success or failure)

---
+++ InTheGymTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,81 @@
+//
+//  FeedLoaderCacheDecoratorTests.swift
+//  InTheGymTests
+//
+//  Created by Findlay Wood on 02/06/2024.
+//  Copyright © 2024 FindlayWood. All rights reserved.
+//
+
+import XCTest
+import ITGWorkoutKit
+
+final class FeedLoaderCacheDecorator: WorkoutLoader {
+    private let decoratee: WorkoutLoader
+
+    init(decoratee: WorkoutLoader) {
+        self.decoratee = decoratee
+    }
+
+    func load(completion: @escaping (WorkoutLoader.Result) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+class FeedLoaderCacheDecoratorTests: XCTestCase {
+
+    func test_load_deliversFeedOnLoaderSuccess() {
+        let feed = uniqueFeed()
+        let loader = LoaderStub(result: .success(feed))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+
+        expect(sut, toCompleteWith: .success(feed))
+    }
+
+    func test_load_deliversErrorOnLoaderFailure() {
+        let loader = LoaderStub(result: .failure(anyNSError()))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+
+        expect(sut, toCompleteWith: .failure(anyNSError()))
+    }
+
+    // MARK: - Helpers
+
+    private func expect(_ sut: WorkoutLoader, toCompleteWith expectedResult: WorkoutLoader.Result, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+
+            case (.failure, .failure):
+                break
+
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    private func uniqueFeed() -> [WorkoutItem] {
+        return [WorkoutItem(id: UUID(), description: "any", location: "any", image: anyURL())]
+    }
+
+    private class LoaderStub: WorkoutLoader {
+        private let result: WorkoutLoader.Result
+
+        init(result: WorkoutLoader.Result) {
+            self.result = result
+        }
+
+        func load(completion: @escaping (WorkoutLoader.Result) -> Void) {
+            completion(result)
+        }
+    }
+
+
+}
