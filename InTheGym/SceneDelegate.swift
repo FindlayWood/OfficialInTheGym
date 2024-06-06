283b320d53246d135884c8bc7e600aabbadd560e
FindlayWood <findlaywood1@gmail.com>
2024-06-06 23:11:03 +0100

    Add #if DEBUG compilation directive to prevent test-specific code from being deployed in production

--- InTheGym/SceneDelegate.swift
+++ InTheGym/SceneDelegate.swift
@@ -40,9 +40,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .defaultDirectoryURL()
             .appendingPathComponent("feed-store.sqlite")
         
+        #if DEBUG
         if CommandLine.arguments.contains("-reset") {
             try? FileManager.default.removeItem(at: localStoreURL)
         }
+        #endif
 
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
@@ -133,29 +135,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     
     private func makeRemoteHTTPClient() -> HTTPClient {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline":
+        #if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
             return AlwaysFailingHTTPClient()
-
-        default:
-            return URLSessionHTTPClient2(session: URLSession(configuration: .ephemeral))
         }
+        #endif
+        return URLSessionHTTPClient2(session: URLSession(configuration: .ephemeral))
     }
     
     
     private func makeRemoteClient() -> Client {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline":
+        #if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
             return AlwaysFailingClient()
-
-        default:
-            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         }
+        #endif
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
 }
 
 extension RemoteLoader: ITGWorkoutKit.WorkoutLoader where Resource == [WorkoutItem] {}
 
+#if DEBUG
 private class AlwaysFailingHTTPClient: HTTPClient {
     private class Task: HTTPClientTask {
         func cancel() {}
@@ -177,3 +178,4 @@ private class AlwaysFailingClient: Client {
         return Task()
     }
 }
+#endif
