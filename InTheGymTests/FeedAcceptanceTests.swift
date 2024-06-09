dd156c890723af4bd87e3b130a25c9f2b7f4863d
FindlayWood <findlaywood1@gmail.com>
2024-06-09 22:17:51 +0100

    Move HTTPClientStub & ClientStub to a shared scope in a separate file

--- InTheGymTests/FeedAcceptanceTests.swift
+++ InTheGymTests/FeedAcceptanceTests.swift
@@ -78,56 +78,6 @@ class FeedAcceptanceTests: XCTestCase {
         sut.sceneWillResignActive(UIApplication.shared.connectedScenes.first!)
     }
     
-    private class ClientStub: Client {
-        private class Task: HTTPClientTask {
-            func cancel() {}
-        }
-
-        private let stub: (URL) -> Client.Result
-
-        init(stub: @escaping (URL) -> Client.Result) {
-            self.stub = stub
-        }
-
-        func get(from path: String, completion: @escaping (Client.Result) -> Void) -> HTTPClientTask {
-            completion(stub(URL(string: path)!))
-            return Task()
-        }
-
-        static var offline: ClientStub {
-            ClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
-        }
-
-        static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> ClientStub {
-            ClientStub { url in .success(stub(url)) }
-        }
-    }
-
-    private class HTTPClientStub: HTTPClient {
-        private class Task: HTTPClientTask {
-            func cancel() {}
-        }
-
-        private let stub: (URL) -> HTTPClient.Result
-
-        init(stub: @escaping (URL) -> HTTPClient.Result) {
-            self.stub = stub
-        }
-
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            completion(stub(url))
-            return Task()
-        }
-
-        static var offline: HTTPClientStub {
-            HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
-        }
-
-        static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> HTTPClientStub {
-            HTTPClientStub { url in .success(stub(url)) }
-        }
-    }
-
     private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
         private(set) var feedCache: CachedFeed?
         private var feedImageDataCache: [URL: Data] = [:]
