efc544c7e42bbd8b9d86968f19859255919abf79
FindlayWood <findlaywood1@gmail.com>
2024-06-10 22:05:50 +0100

    Record feed with failed image loading snapshot

--- ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedSnapshotTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedSnapshotTests.swift
@@ -35,6 +35,14 @@ final class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+
+        sut.display(feedWithFailedImageLoading())
+
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT() -> FeedViewController {
@@ -60,6 +68,21 @@ final class FeedSnapshotTests: XCTestCase {
                 description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
                 location: "Garth Pier",
                 image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil
             )
         ]
     }
