d928193c27ca7542be162a9fb2b0be6285d89069
FindlayWood <findlaywood1@gmail.com>
2024-06-10 21:51:39 +0100

    Record feed with content snapshot

--- ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedSnapshotTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOSTests/FeedSnapshotTests.swift
@@ -7,61 +7,117 @@
 
 import XCTest
 import ITGWorkoutKitiOS
+@testable import ITGWorkoutKit
 
 final class FeedSnapshotTests: XCTestCase {
+    
+    func test_emptyFeed() {
+        let sut = makeSUT()
+        
+        sut.display(emptyFeed())
+        
+        record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+    }
+    
+    func test_feedWithContent() {
+        let sut = makeSUT()
 
-        func test_emptyFeed() {
-            let sut = makeSUT()
+        sut.display(feedWithContent())
 
-            sut.display(emptyFeed())
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! FeedViewController
+        controller.loadViewIfNeeded()
+        return controller
+    }
+    
+    private func emptyFeed() -> [FeedImageCellController] {
+        return []
+    }
+    
+    private func feedWithContent() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+    
+    private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
+        guard let snapshotData = snapshot.pngData() else {
+            XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+            return
+        }
+        
+        let snapshotURL = URL(fileURLWithPath: String(describing: file))
+            .deletingLastPathComponent()
+            .appendingPathComponent("snapshots")
+            .appendingPathComponent("\(name).png")
+        
+        do {
+            try FileManager.default.createDirectory(
+                at: snapshotURL.deletingLastPathComponent(),
+                withIntermediateDirectories: true
+            )
+            
+            try snapshotData.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+        }
+    }
+    
+}
 
-            record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+extension UIViewController {
+    func snapshot() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+        return renderer.image { action in
+            view.layer.render(in: action.cgContext)
+        }
+    }
+}
+
+private extension FeedViewController {
+    func display(_ stubs: [ImageStub]) {
+        let cells: [FeedImageCellController] = stubs.map { stub in
+            let cellController = FeedImageCellController(delegate: stub)
+            stub.controller = cellController
+            return cellController
         }
 
-        // MARK: - Helpers
+        display(cells)
+    }
+}
 
-        private func makeSUT() -> FeedViewController {
-            let bundle = Bundle(for: FeedViewController.self)
-            let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-            let controller = storyboard.instantiateInitialViewController() as! FeedViewController
-            controller.loadViewIfNeeded()
-            return controller
-        }
+private class ImageStub: FeedImageCellControllerDelegate {
+    let viewModel: FeedImageViewModel<UIImage>
+    weak var controller: FeedImageCellController?
 
-        private func emptyFeed() -> [FeedImageCellController] {
-            return []
-        }
-
-        private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
-            guard let snapshotData = snapshot.pngData() else {
-                XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
-                return
-            }
-
-            let snapshotURL = URL(fileURLWithPath: String(describing: file))
-                .deletingLastPathComponent()
-                .appendingPathComponent("snapshots")
-                .appendingPathComponent("\(name).png")
-
-            do {
-                try FileManager.default.createDirectory(
-                    at: snapshotURL.deletingLastPathComponent(),
-                    withIntermediateDirectories: true
-                )
-
-                try snapshotData.write(to: snapshotURL)
-            } catch {
-                XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
-            }
-        }
-
+    init(description: String?, location: String?, image: UIImage?) {
+        viewModel = FeedImageViewModel(
+            description: description,
+            location: location,
+            image: image,
+            isLoading: false,
+            shouldRetry: image == nil)
     }
 
-    extension UIViewController {
-        func snapshot() -> UIImage {
-            let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
-            return renderer.image { action in
-                view.layer.render(in: action.cgContext)
-            }
-        }
+    func didRequestImage() {
+        controller?.display(viewModel)
     }
+
+    func didCancelImageRequest() {}
+}
