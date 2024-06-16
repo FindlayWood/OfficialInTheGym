beeac8412620579020041c8ff5da6d6635f52d86
FindlayWood <findlaywood1@gmail.com>
2024-06-16 20:12:14 +0100

    Replace RemoteFeedImageDataLoader with HTTPClient publisher composed with FeedImageDataMapper

--- ITGWorkoutKit/WorkoutAPIEndToEndTests/WorkoutAPIEndToEndTests.swift
+++ ITGWorkoutKit/WorkoutAPIEndToEndTests/WorkoutAPIEndToEndTests.swift
@@ -70,14 +70,23 @@ final class WorkoutAPIEndToEndTests: XCTestCase {
     }
     
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-        let loader = RemoteFeedImageDataLoader(client: ephemeralHTTPClient())
-        trackForMemoryLeaks(loader, file: file, line: line)
 
+        let client = ephemeralHTTPClient()
+        
         let exp = expectation(description: "Wait for load completion")
 
+        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
+        
         var receivedResult: FeedImageDataLoader.Result?
-        _ = loader.loadImageData(from: feedTestServerURL) { result in
-            receivedResult = result
+        
+        client.get(from: url) { result in
+            receivedResult = result.flatMap { (data, response) in
+                do {
+                    return .success(try FeedImageDataMapper.map(data, from: response))
+                } catch {
+                    return .failure(error)
+                }
+            }
             exp.fulfill()
         }
         wait(for: [exp], timeout: 5.0)
--- InTheGym/Feed Feature/CombineHelpers.swift
+++ InTheGym/Feed Feature/CombineHelpers.swift
@@ -26,6 +26,22 @@ public extension Client {
     }
 }
 
+public extension HTTPClient {
+    typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
+
+    func getPublisher(url: URL) -> Publisher {
+        var task: HTTPClientTask?
+
+        return Deferred {
+            Future { completion in
+                task = self.get(from: url, completion: completion)
+            }
+        }
+        .handleEvents(receiveCancel: { task?.cancel() })
+        .eraseToAnyPublisher()
+    }
+}
+
 public extension LocalFeedLoader {
     typealias Publisher = AnyPublisher<[WorkoutItem], Error>
 
