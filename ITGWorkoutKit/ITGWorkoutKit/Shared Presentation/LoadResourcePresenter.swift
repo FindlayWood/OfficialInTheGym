9888a60ca2780d63a12530e9e97652d95e986062
FindlayWood <findlaywood1@gmail.com>
2024-06-17 19:45:35 +0100

    Remove title from generic presenter since it's specific to each presenter

--- ITGWorkoutKit/ITGWorkoutKit/Shared Presentation/LoadResourcePresenter.swift
+++ ITGWorkoutKit/ITGWorkoutKit/Shared Presentation/LoadResourcePresenter.swift
@@ -25,13 +25,6 @@ public final class LoadResourcePresenter {
         self.errorView = errorView
     }
     
-    public static var title: String {
-        return NSLocalizedString("FEED_VIEW_TITLE",
-            tableName: "Feed",
-            bundle: Bundle(for: FeedPresenter.self),
-            comment: "Title for the feed view")
-    }
-    
     public func didStartLoadingFeed() {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
--- ITGWorkoutKit/ITGWorkoutKitTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ ITGWorkoutKit/ITGWorkoutKitTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -10,10 +10,6 @@ import ITGWorkoutKit
 
 final class LoadResourcePresenterTests: XCTestCase {
 
-    func test_title_isLocalized() {
-        XCTAssertEqual(LoadResourcePresenter.title, localized("FEED_VIEW_TITLE"))
-    }
-
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
 
