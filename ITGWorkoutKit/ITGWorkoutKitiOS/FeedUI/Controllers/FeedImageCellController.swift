1288a115af62d852fc8fe8d262fd2889db627373
FindlayWood <findlaywood1@gmail.com>
2024-06-24 20:57:07 +0100

    Weakify self within onRetry closure to prevent cell from holding strong references to other components

--- ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedImageCellController.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/FeedUI/Controllers/FeedImageCellController.swift
@@ -38,7 +38,9 @@ extension FeedImageCellController: UITableViewDataSource, UITableViewDelegate, U
         cell?.locationLabel.text = viewModel.location
         cell?.descriptionLabel.text = viewModel.description
         cell?.feedImageView.image = nil
-        cell?.onRetry = delegate.didRequestImage
+        cell?.onRetry = { [weak self] in
+            self?.delegate.didRequestImage()
+        }
         delegate.didRequestImage()
         
         cell?.onReuse = { [weak self] in
