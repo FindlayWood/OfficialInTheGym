b6c13445e5016e78f0c752fcdb1244022a516df7
FindlayWood <findlaywood1@gmail.com>
2024-06-28 22:44:50 +0100

    Set data source default row animation to fade

--- ITGWorkoutKit/ITGWorkoutKitiOS/Shared UI/Controllers/ListViewController.swift
+++ ITGWorkoutKit/ITGWorkoutKitiOS/Shared UI/Controllers/ListViewController.swift
@@ -24,6 +24,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
         
+        dataSource.defaultRowAnimation = .fade
         tableView.dataSource = dataSource
         configureErrorView()
         
