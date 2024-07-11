bdd124b1c5a904c61d7c559dd71cd96021ff5692
FindlayWood <findlaywood1@gmail.com>
2024-07-11 21:39:10 +0100

    Dispatches from background to main queue

--- InTheGymTests/CommentsUIIntegrationTests.swift
+++ InTheGymTests/CommentsUIIntegrationTests.swift
@@ -95,7 +95,7 @@ final class CommentsUIIntegrationTests: FeedUIIntegrationTests {
          assertThat(sut, isRendering: [comment])
      }
 
-     override func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+     func test_loadCommentsCompletion_dispatchesFromBackgroundToMainThread() {
          let (sut, loader) = makeSUT()
          sut.simulateAppearance()
 
