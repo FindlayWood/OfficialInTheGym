dcc61a38e204d19a252eb7353dfe3e18582ad465
Findlay-Suji <110030465+Findlay-Suji@users.noreply.github.com>
2025-11-14 17:15:40 +0000

    update rep view

--- MyDayKit/MyDayKitUI/Screens/MyDayKitRepsView.swift
+++ MyDayKit/MyDayKitUI/Screens/MyDayKitRepsView.swift
@@ -12,11 +12,7 @@ struct MyDayKitRepsView: View {
     @ObservedObject var dayManager: MyDayManager
     
     let exercise: MyDayNewExerciseManager
-    @State var reps: Int = 0
-    
-    // Range of possible reps
-    let minReps = 0
-    let maxReps = 50
+    @State var reps: Int = 1
     
     var add: (() -> ())?
     
@@ -24,24 +20,87 @@ struct MyDayKitRepsView: View {
         VStack(spacing: 20) {
             Text("How many reps did you complete?")
                 .font(.headline)
+                .padding()
             
             Text(exercise.exercise.name)
                 .font(.title2)
                 .bold()
             
-            Stepper(value: $reps, in: minReps...maxReps) {
-                Text("\(reps) reps")
-                    .font(.title3)
-                    .monospacedDigit()
-            }
-            .padding()
+            Spacer()
             
-            Slider(value: Binding(
-                get: { Double(reps) },
-                set: { reps = Int($0) }
-            ), in: Double(minReps)...Double(maxReps), step: 1)
-            .accentColor(.blue)
+            HStack {
+                Spacer()
+                Button {
+                    guard reps > 0 else { return }
+                    reps -= 1
+                } label: {
+                    Image(systemName: "minus.circle.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .opacity(reps > 0 ? 1 : 0.3)
+                }
+                .disabled(reps < 2)
+                Spacer()
+                Text("\(reps)")
+                    .font(.system(size: 100, weight: .bold))
+                    .foregroundStyle(Color.black)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .minimumScaleFactor(0.5)
+                    .lineLimit(1)
+                Spacer()
+                Button {
+                    guard reps < 99 else { return }
+                    reps += 1
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .opacity(reps < 99 ? 1 : 0.3)
+                }
+                .disabled(reps > 98)
+                Spacer()
+            }
             .padding(.horizontal)
+            
+            
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(1..<100, id: \.self) { num in
+                            Button {
+                                reps = num
+                                withAnimation {
+                                    proxy.scrollTo("\(num)", anchor: .center)
+                                }
+                            } label: {
+                                Text("\(num)")
+                                    .font(.system(size: 30, weight: .bold))
+                                    .foregroundStyle(num == reps ? Color.white : Color.black.opacity(0.5))
+                                    .frame(width: 80, height: 80)
+                                    .background {
+                                        Circle()
+                                            .foregroundStyle(Color.blue.opacity(num == reps ? 1 : 0.3))
+                                    }
+                                    .overlay {
+                                        Circle()
+                                            .inset(by: 1)
+                                            .stroke(Color.black, lineWidth: 1)
+                                    }
+                            }
+                            .padding(.vertical)
+                            .id("\(num)")
+                        }
+                    }
+                    .padding(.leading)
+                }
+                .onChange(of: reps) { _, newValue in
+                    withAnimation {
+                        proxy.scrollTo("\(newValue)", anchor: .center)
+                    }
+                }
+            }
+
+            Spacer()
             
             Button {
                 addAction()
@@ -56,8 +115,8 @@ struct MyDayKitRepsView: View {
                             .clipShape(RoundedRectangle(cornerRadius: 10))
                     }
             }
+            .padding()
         }
-        .padding()
     }
     
     func addAction() {
